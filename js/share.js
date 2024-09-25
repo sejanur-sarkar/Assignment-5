9775c4ffea7b602c4bc92d935ca8cef932bb0b49
sejanur-sarkar <sejanursarkar5336@gmail.com>
2024-09-25 19:19:24 +0600

    added the code

--- js/share.js
+++ js/share.js
@@ -1,15 +1,33 @@
-function getInputFieldValueById(id){
+function getInputFieldValueById(id) {
     const inputValue = document.getElementById(id).value;
-    
+
     const inputNumber = parseFloat(inputValue);
-   
+
     return inputNumber;
 
 }
-function getTextFieldValueById(id){
+function getTextFieldValueById(id) {
     const textValue = document.getElementById(id).textContent;
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
 
+document.getElementById('donate-btn')
+    .addEventListener('click', function () {
 
+        document.getElementById('donate-btn').classList.add('bg-[#B4F461]');
+        document.getElementById('donate-btn').classList.remove('bg-white', 'text-[#111111B3]');
+        document.getElementById('history-btn').classList.remove('bg-[#B4F461]', 'text-black');
+        document.getElementById('history-btn').classList.add('bg-white', 'border');
+        document.getElementById('donate').classList.remove('hidden')
+        document.getElementById('history').classList.add('hidden')
+    })
+document.getElementById('history-btn')
+    .addEventListener('click', function () {
+        document.getElementById('history-btn').classList.add('bg-[#B4F461]', 'text-black');
+        document.getElementById('history-btn').classList.remove('bg-white', 'border');
+        document.getElementById('donate-btn').classList.add('bg-white', 'text-[#111111B3]', 'border');
+        document.getElementById('donate-btn').classList.remove('bg-[#B4F461]');
+        document.getElementById('history').classList.remove('hidden')
+        document.getElementById('donate').classList.add('hidden')
+    })
